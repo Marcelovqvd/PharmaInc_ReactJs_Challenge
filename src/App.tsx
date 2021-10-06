4d37abcf3ba5f7ad8d90b643629795139579a045
Marcelovqvd <marcelovqvd@gmail.com>
2021-10-06 16:09:49 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Pharma Inc</h1>
+    </div>
+  );
+}
+
+export default App;
