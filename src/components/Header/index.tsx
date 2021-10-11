7d44dfba13de73d4c50f960268c55871e21fbe42
Marcelovqvd <marcelovqvd@gmail.com>
2021-10-11 16:04:23 -0300

    Header; Material UI

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,37 @@
-import logoImg from '../../assets/healthcare.svg';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
-export function Header() {
+export default function Header() {
   return (
-    <>
-      <img src={logoImg} alt="logo" />
-      <h1>COMPANY</h1>
-    </>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <Stack direction="row" spacing={2}>
+    </Stack> 
+      <AppBar position="static">
+        <Toolbar>          
+          <LocalHospitalIcon></LocalHospitalIcon>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Company
+          </Typography>          
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+              <AccountCircle />
+              </IconButton>              
+            </div>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
