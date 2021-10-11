dbbf1a3f2c90429b0c361563d257f854917c2b3f
Marcelovqvd <marcelovqvd@gmail.com>
2021-10-11 16:32:30 -0300

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import MainText from './components/MainText';
+import BasicTextFields from './components/Input';
 
 function App() {
   return (
     <>
       <Header />
       <MainText />
+      <BasicTextFields />
     </>
   )
 }
