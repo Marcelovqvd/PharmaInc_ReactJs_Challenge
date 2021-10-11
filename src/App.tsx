dcc3cb72043ac4270ac0c064b715480c8368ef1d
Marcelovqvd <marcelovqvd@gmail.com>
2021-10-11 16:24:56 -0300

    Main Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
+import MainText from './components/MainText';
 
 function App() {
   return (
     <>
       <Header />
+      <MainText />
     </>
   )
 }
