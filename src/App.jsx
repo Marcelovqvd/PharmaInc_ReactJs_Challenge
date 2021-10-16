10a7671d7e324e163e60ac5cb72245c77bd22948
Marcelovqvd <marcelovqvd@gmail.com>
2021-10-16 16:15:53 -0300

    styles, header and main text

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import MainText from './components/MainText';
-import BasicTextFields from './components/Input';
-import BasicTable from './components/Table';
-import UsersList from './components/UsersList';
 
 function App() {
   return (
     <>
       <Header />
       <MainText />
-      <BasicTextFields />
-      <BasicTable />
-      <UsersList />
     </>
   )
 }
